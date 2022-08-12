b396007b0db6e10088a8c2870faff8ce2edc7968
Agong <58037317+kim946509@users.noreply.github.com>
2022-08-12 17:07:21 +0900

    Add files via upload

---
+++ 6043.py
@@ -0,0 +1,3 @@
+f1, f2 = input().split()
+value = format(float(f1)/float(f2), ".3f")
+print(value)
---
+++ 6045.py
@@ -0,0 +1,7 @@
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+sum = a+b+c
+quota = sum/3
+print(sum, format(quota, "0.2f"))
